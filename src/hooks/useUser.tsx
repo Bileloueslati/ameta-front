0c8eedd5115af46adf583b5240a1f41efc2832f6
Bilel <b.oueslati@ewm.swiss>
2022-10-08 03:05:09 +0200

    Update user hook

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,15 +1,18 @@
 import { useAuth } from "./useAuth";
 import jwtDecode from "jwt-decode";
 import { User } from "../__typescript/api";
+import { useMemo } from "react";
 
 const useUser = () => {
   const { firstName, lastName, token, update } = useAuth();
 
   if (!token || !firstName || !lastName) {
-    throw new Error("User is not authenticated");
+    throw new Error(
+      "User is not authenticated, Make sure you use this hook inside <PrivateRoute />"
+    );
   }
 
-  const { id, roles } = jwtDecode<User>(token);
+  const { id, roles } = useMemo(() => jwtDecode<User>(token), [token]);
 
   const isAdmin = roles.includes("ROLE_ADMIN");
 
