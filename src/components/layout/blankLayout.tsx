267e0c3c5f14e259691b01af651c8f77de747595
Bilel <b.oueslati@ewm.swiss>
2022-10-12 12:37:06 +0200

    Update layout

--- src/components/layout/blankLayout.tsx
+++ src/components/layout/blankLayout.tsx
@@ -1,5 +1,4 @@
 import { Stack } from "@mui/material";
-import { FunctionComponent, PropsWithChildren } from "react";
 import { Outlet } from "react-router-dom";
 import useThemeMode from "../../hooks/useThemeMode";
 
@@ -13,8 +12,7 @@ const BlankLayout = () => {
         minHeight: "100vh",
         width: "100%",
         overflow: "hidden",
-        backgroundColor:
-          mode == "light" ? "rgba(238, 238, 238, 0.5)" : "background.default",
+        backgroundColor: "background.default",
       }}
     >
       <Outlet />
