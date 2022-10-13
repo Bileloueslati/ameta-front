f93b98375c5a934204308a939d38fbb864d30787
Bilel <b.oueslati@ewm.swiss>
2022-10-14 01:45:35 +0200

    Update User roles HOC

--- src/hoc/withRoles.tsx
+++ src/hoc/withRoles.tsx
@@ -5,13 +5,21 @@ import intersection from "lodash/intersection";
 import RestrictedRoute from "../components/routes/restrictedRoute";
 
 const WithRoles =
-  <T extends Object>(Component: ComponentType<T>, roles: UserRoles) =>
+  <T extends Object>(
+    Component: ComponentType<T>,
+    roles: UserRoles,
+    showError: boolean = true
+  ) =>
   (props: T) => {
     const { roles: userRoles } = useUser();
 
     const isAllowed = !!intersection(roles, userRoles).length;
 
-    if (!isAllowed) return <RestrictedRoute />;
+    if (!isAllowed) {
+      if (showError) return <RestrictedRoute />;
+
+      return null;
+    }
 
     return <Component {...props} />;
   };
