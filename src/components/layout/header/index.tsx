18a3ee103fd2d764c3d9112186f2cdf148e8043c
Bilel <b.oueslati@ewm.swiss>
2022-10-13 12:16:04 +0200

    Update layout

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,46 +1,40 @@
 import { Stack, Container, Box, Paper, Link } from "@mui/material";
-import useThemeMode from "../../../hooks/useThemeMode";
+import { FC } from "react";
 import Logo from "../../logo";
 import Nav from "../sidebar/nav";
 import FullScreen from "./fullScreen";
-import NavExpander from "./navExpander";
 import Notifications from "./notifications";
-import Search from "./search";
 import ThemeModeSwitcher from "./themeMode";
 import UserDropDown from "./userDropDown";
 
-export default function Header() {
-  const { mode } = useThemeMode();
-
-  return (
-    <Paper
-      elevation={1}
-      sx={({ palette }) => ({
-        width: "100%",
-        py: 0.5,
-        mb: 4,
-      })}
-    >
-      <Container maxWidth={"xxl"}>
-        <Stack justifyContent="space-between" direction="row">
-          <Stack direction="row" spacing={4} alignItems="center">
-            <Link href="/">
-              <Logo sx={{ maxWidth: 260 }} />
-            </Link>
-
-            <Box>
-              <Nav />
-            </Box>
-          </Stack>
-
-          <Stack direction="row" spacing={2} alignItems="center">
-            <FullScreen />
-            <ThemeModeSwitcher />
-            <Notifications />
-            <UserDropDown />
-          </Stack>
+const Header: FC = () => (
+  <Paper
+    elevation={1}
+    sx={{
+      width: "100%",
+      py: 0.5,
+      mb: 4,
+    }}
+  >
+    <Container maxWidth={"xxl"}>
+      <Stack justifyContent="space-between" direction="row">
+        <Stack direction="row" spacing={4} alignItems="center">
+          <Link href="/">
+            <Logo sx={{ maxWidth: 260 }} />
+          </Link>
+          <Box>
+            <Nav />
+          </Box>
         </Stack>
-      </Container>
-    </Paper>
-  );
-}
+        <Stack direction="row" spacing={2} alignItems="center">
+          <FullScreen />
+          <ThemeModeSwitcher />
+          <Notifications />
+          <UserDropDown />
+        </Stack>
+      </Stack>
+    </Container>
+  </Paper>
+);
+
+export default Header;
